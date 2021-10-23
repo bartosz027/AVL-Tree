ea8acb647ae75afce69d6afd46cc96952a944977
Bartosz <nieznajomy027@gmail.com>
2021-10-24 00:11:35 +0200

    Added existing project

---
+++ AVL/src/AVL/AVL.h
@@ -0,0 +1,309 @@
+#pragma once
+#include <vector>
+
+template<typename T>
+struct Node {
+	T Key;
+
+	int Height;
+	int Balance;
+
+	Node* Left;
+	Node* Right;
+};
+
+template<typename T>
+class AVL {
+public:
+	AVL() :
+		m_Root(nullptr), m_ModifyTree(true) {
+	}
+
+	void Insert(T value);
+	void Delete(T value);
+
+	Node<T>* Search(T value) const;
+	std::vector<Node<T>*> GetPreorderTraversal() const;
+private:
+	Node<T>* Create(T value);
+
+	void Insert(Node<T>*& node, T value);
+	void Delete(Node<T>*& node, T value);
+
+	Node<T>* Search(Node<T>* node, T value) const;
+	void PreorderTraversal(Node<T>* node, std::vector<Node<T>*>& klp) const;
+
+	Node<T>* LR(Node<T>* node) const;
+	Node<T>* RL(Node<T>* node) const;
+
+	Node<T>* LL(Node<T>* node) const;
+	Node<T>* RR(Node<T>* node) const;
+
+	void CalculateHeight(Node<T>* node) const;
+	void CalculateBalance(Node<T>* node) const;
+private:
+	Node<T>* m_Root;
+	bool m_ModifyTree;
+};
+
+
+template<typename T>
+inline void AVL<T>::Insert(T value) {
+	m_ModifyTree = true;
+	Insert(m_Root, value);
+}
+
+template<typename T>
+inline void AVL<T>::Insert(Node<T>*& node, T value) {
+	if(!node) {
+		node = Create(value);
+		return;
+	}
+	else if (value < node->Key) {
+		Insert(node->Left, value);
+	}
+	else if (value > node->Key) {
+		Insert(node->Right, value);
+	}
+
+	if (m_ModifyTree) {
+		CalculateHeight(node);
+		CalculateBalance(node);
+
+		if (node->Balance == -2) {
+			if (node->Right->Balance == -1) {
+				node = LL(node);
+			}
+			else {
+				node = RL(node);
+			}
+
+			m_ModifyTree = false;
+		}
+
+		if (node->Balance == 2) {
+			if (node->Left->Balance == 1) {
+				node = RR(node);
+			}
+			else {
+				node = LR(node);
+			}
+
+			m_ModifyTree = false;
+		}
+
+		if (node->Balance == 0) {
+			m_ModifyTree = false;
+		}
+	}
+}
+
+template<typename T>
+inline Node<T>* AVL<T>::Create(T value) {
+	Node<T>* node = new Node<T>();
+	node->Key = value;
+
+	node->Height = 0;
+	node->Balance = 0;
+
+	node->Left = nullptr;
+	node->Right = nullptr;
+
+	return node;
+}
+
+
+template<typename T>
+inline void AVL<T>::Delete(T value) {
+	m_ModifyTree = true;
+	Delete(m_Root, value);
+}
+
+template<typename T>
+inline void AVL<T>::Delete(Node<T>*& node, T value) {
+	if (!node) {
+		return;
+	}
+	
+	if (value == node->Key) {
+		if (node->Left && node->Right) {
+			Node<T>* iterator = node->Left;
+
+			while (iterator->Right) {
+				iterator = iterator->Right;
+			}
+
+			node->Key = iterator->Key;
+			Delete(node->Left, iterator->Key);
+		}
+		else if (node->Left) {
+			node = node->Left;
+		}
+		else if (node->Right) {
+			node = node->Right;
+		}
+		else {
+			node = nullptr;
+			return;
+		}
+	}
+	else if (value < node->Key) {
+		Delete(node->Left, value);
+	}
+	else if (value > node->Key) {
+		Delete(node->Right, value);
+	}
+
+	if (m_ModifyTree) {
+		CalculateHeight(node);
+		CalculateBalance(node);
+
+		if (node->Balance == -2) {
+			if (node->Right->Balance == -1 || node->Right->Balance == 0) {
+				node = LL(node);
+			}
+			else {
+				node = RL(node);
+			}
+		}
+
+		if (node->Balance == 2) {
+			if (node->Left->Balance == 1 || node->Left->Balance == 0) {
+				node = RR(node);
+			}
+			else {
+				node = LR(node);
+			}
+		}
+
+		if (node->Balance == -1 || node->Balance == 1) {
+			m_ModifyTree = false;
+		}
+	}
+}
+
+
+template<typename T>
+inline Node<T>* AVL<T>::Search(T value) const {
+	return Search(m_Root, value);
+}
+
+template<typename T>
+inline Node<T>* AVL<T>::Search(Node<T>* node, T value) const {
+	if (!node) {
+		return nullptr;
+	}
+	else if (value < node->Key) {
+		return Search(node->Left, value);
+	}
+	else if (value > node->Key) {
+		return Search(node->Right, value);
+	}
+	else {
+		return node;
+	}
+}
+
+
+template<typename T>
+std::vector<Node<T>*> AVL<T>::GetPreorderTraversal() const {
+	if (m_Root) {
+		std::vector<Node<T>*> klp;
+		PreorderTraversal(m_Root, klp);
+
+		return klp;
+	}
+
+	return std::vector<Node<T>*>();
+}
+
+template<typename T>
+void AVL<T>::PreorderTraversal(Node<T>* node, std::vector<Node<T>*>& klp) const {
+	if (node) {
+		klp.push_back(node);
+		PreorderTraversal(node->Left, klp);
+		PreorderTraversal(node->Right, klp);
+	}
+}
+
+
+template<typename T>
+inline Node<T>* AVL<T>::LR(Node<T>* node) const {
+	node->Left = LL(node->Left);
+	return RR(node);
+}
+
+template<typename T>
+inline Node<T>* AVL<T>::RL(Node<T>* node) const {
+	node->Right = RR(node->Right);
+	return LL(node);
+}
+
+template<typename T>
+inline Node<T>* AVL<T>::LL(Node<T>* node) const {
+	Node<T>* A = node;
+	Node<T>* B = node->Right;
+	Node<T>* II = B->Left;
+
+	B->Left = A;
+	A->Right = II;
+
+	CalculateHeight(A);
+	CalculateBalance(A);
+
+	CalculateHeight(B);
+	CalculateBalance(B);
+
+	return B;
+}
+
+template<typename T>
+inline Node<T>* AVL<T>::RR(Node<T>* node) const {
+	Node<T>* A = node;
+	Node<T>* B = node->Left;
+	Node<T>* II = B->Right;
+
+	A->Left = II;
+	B->Right = A;
+
+	CalculateHeight(A);
+	CalculateBalance(A);
+
+	CalculateHeight(B);
+	CalculateBalance(B);
+
+	return B;
+}
+
+
+template<typename T>
+inline void AVL<T>::CalculateHeight(Node<T>* node) const {
+	if (node->Left && node->Right) {
+		node->Height = std::max(node->Left->Height, node->Right->Height) + 1;
+	}
+	else if (node->Left) {
+		node->Height = node->Left->Height + 1;
+	}
+	else if (node->Right) {
+		node->Height = node->Right->Height + 1;
+	}
+	else {
+		node->Height = 0;
+	}
+}
+
+template<typename T>
+inline void AVL<T>::CalculateBalance(Node<T>* node) const {
+	if (node->Left && node->Right) {
+		node->Balance = node->Left->Height - node->Right->Height;
+	}
+	else if (node->Left) {
+		node->Balance = node->Left->Height + 1;
+	}
+	else if (node->Right) {
+		node->Balance = -1 * (node->Right->Height + 1);
+	}
+	else {
+		node->Balance = 0;
+	}
+}
---
+++ AVL/src/Example.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+
+#include "AVL/AVL.h"
+
+int main() {
+	AVL<int> AVL_Tree;
+	std::vector<Node<int>*> preorder;
+
+	// Insert
+	AVL_Tree.Insert(21);
+	AVL_Tree.Insert(98);
+	AVL_Tree.Insert(86);
+	AVL_Tree.Insert(36);
+	AVL_Tree.Insert(27);
+	AVL_Tree.Insert(53);
+	AVL_Tree.Insert(48);
+	AVL_Tree.Insert(55);
+	AVL_Tree.Insert(52);
+	AVL_Tree.Insert(40);
+	AVL_Tree.Insert(22);
+	AVL_Tree.Insert(1);
+	AVL_Tree.Insert(9);
+	AVL_Tree.Insert(7);
+	AVL_Tree.Insert(2);
+
+	// Print insert
+	std::cout << "AVL tree nodes:\n";
+	std::cout << "> ";
+
+	preorder = AVL_Tree.GetPreorderTraversal();
+
+	for (const auto& data : preorder) {
+		std::cout << data->Key << " [" << data->Balance << "] ";
+	}
+
+	std::cout << '\n';
+
+	// Delete
+	AVL_Tree.Delete(2);
+	AVL_Tree.Delete(48);
+	AVL_Tree.Delete(98);
+	AVL_Tree.Delete(22);
+	AVL_Tree.Delete(1);
+
+	// Print delete
+	std::cout << "AVL tree nodes:\n";
+	std::cout << "> ";
+
+	preorder = AVL_Tree.GetPreorderTraversal();
+
+	for (const auto& data : preorder) {
+		std::cout << data->Key << " [" << data->Balance << "] ";
+	}
+
+	std::cout << '\n';
+
+	// Search node1
+	const auto& node1 = AVL_Tree.Search(55);
+	std::cout << "Node1 Key:\n";
+
+	if (node1) {
+		std::cout << "> " << node1->Key << '\n';
+	}
+	else {
+		std::cout << "> Node1 not found!\n";
+	}
+
+	// Search node2
+	const auto& node2 = AVL_Tree.Search(2137);
+	std::cout << "Node2 Key:\n";
+
+	if (node2) {
+		std::cout << "> " << node2->Key << '\n';
+	}
+	else {
+		std::cout << "> Node2 not found!\n";
+	}
+
+	return 0;
+}
